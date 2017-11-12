4a9fe0cc986a1cfbb8505dfe2d8a43349af5bdd1
mchieco <31969451+mchieco@users.noreply.github.com>
2017-11-12 12:34:15 -0500

    fixed merge crap

--- src/hackathon17/app.java
+++ src/hackathon17/app.java
@@ -7,19 +7,9 @@ import java.applet.*;
 
 
 public class app extends Canvas {
-<<<<<<< HEAD
-	private MainPanel menu;
-	public app(){
-		menu = new MainPanel();
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new app();
-=======
 	private static MainPanel bet;
 	private int windowHeight, windowWidth;
 	JFrame mainScreen;
-	
 	public app() {
 		windowHeight = 720;
     	windowWidth = 1280;		
@@ -28,7 +18,6 @@ public class app extends Canvas {
         mainScreen.getContentPane().add(this, BorderLayout.CENTER);
         mainScreen.setSize(new Dimension(windowWidth,windowHeight));
         mainScreen.setVisible(true);
->>>>>>> 734ec2d4ee547d07b6a818511267b4dd07f623a1
 	}
 	
     public static void main(String ad[])
