7e13b8fdc534e5991d9a8d5e65a684aa109498bf
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2017-11-12 14:22:47 -0500

    bet323423

--- src/hackathon17/ImportantNumbers.java
+++ src/hackathon17/ImportantNumbers.java
@@ -11,6 +11,7 @@ public class ImportantNumbers {
 	public void render(Graphics g) {
 		
 		Font font = new Font("impact", 1, 33);
+		Font font1 = new Font("impact", 1, 50);
 		
 		g.setFont(font);
 		g.setColor(Color.red);
@@ -22,5 +23,10 @@ public class ImportantNumbers {
 		g.drawString("Nearby Hospitals", 100, 310);
 		g.drawString("Nearby Water Company", 100, 340);
 		g.drawString("Nearby Power Company", 100, 370);
+		
+		g.drawRect(100, 500, 100, 150);
+		g.setFont(font1);
+		g.setColor(Color.red);
+		g.drawString("Back", 100, 150);
 	}
 }
