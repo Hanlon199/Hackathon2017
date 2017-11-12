f4673dbfd73ff91d2293b1232acfe2a4e7edb9b3
mchieco <31969451+mchieco@users.noreply.github.com>
2017-11-12 12:29:53 -0500

    cool beans

--- src/hackathon17/app.java
+++ src/hackathon17/app.java
@@ -10,9 +10,6 @@ public class app extends Canvas {
 	private MainPanel menu;
 	public app(){
 		menu = new MainPanel();
-		BufferStrategy bs = this.getBufferStrategy();
-		Graphics g = bs.getDrawGraphics();
-		menu.render(g);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
